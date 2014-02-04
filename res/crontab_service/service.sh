60cc81decba2e67ffe694500fef6feaa042f08d0
dorimanx <yuri@bynet.co.il>
2014-02-04 11:57:28 +0200

    Fixed CRONTAB activation for 4.2.2 ROMS.

--- res/crontab_service/service.sh
+++ res/crontab_service/service.sh
@@ -2,13 +2,6 @@
 
 # Created By Dorimanx and Dairinin
 
-JELLY=0;
-JB_SAMMY=0;
-[ -f /system/lib/ssl/engines/libkeystore.so ] && JELLY=1;
-if [ -e /tmp/sammy_rom ]; then
-	JB_SAMMY=1;
-fi;
-
 # allow custom user jobs
 if [ ! -e /data/crontab/root ]; then
 	mkdir /data/crontab/;
@@ -17,35 +10,13 @@ if [ ! -e /data/crontab/root ]; then
 	chmod 777 /data/crontab/root;
 fi;
 
-JELLY_MIUI()
-{
-	if [ ! -e /system/etc/cron.d/crontabs/root ]; then
-		mkdir -p /system/etc/cron.d/crontabs/;
-		cp -a /data/crontab/root /system/etc/cron.d/crontabs/;
-		chown 0:0 /system/etc/cron.d/crontabs/*;
-		chmod 777 /system/etc/cron.d/crontabs/*;
-	fi;
-	echo "root:x:0:0::/system/etc/cron.d/crontabs:/sbin/sh" > /etc/passwd;
-}
-
-JB_SAMMY_CRON()
-{
-	if [ ! -e /var/spool/cron/crontabs/root ]; then
-		mkdir -p /var/spool/cron/crontabs/;
-		cp -a /data/crontab/root /var/spool/cron/crontabs/;
-		chown 0:0 /var/spool/cron/crontabs/*;
-		chmod 777 /var/spool/cron/crontabs/*;
-	fi;
-	echo "root:x:0:0::/var/spool/cron/crontabs:/sbin/sh" > /etc/passwd;
-}
-
-if [ "$JB_SAMMY" -eq "1" ]; then
-	JB_SAMMY_CRON;
-elif [ "$JELLY" -eq "1" ]; then
-	JELLY_MIUI;
-else
-	JB_SAMMY_CRON;
+if [ ! -e /var/spool/cron/crontabs/root ]; then
+	mkdir -p /var/spool/cron/crontabs/;
+	cp -a /data/crontab/root /var/spool/cron/crontabs/;
+	chown 0:0 /var/spool/cron/crontabs/*;
+	chmod 777 /var/spool/cron/crontabs/*;
 fi;
+echo "root:x:0:0::/var/spool/cron/crontabs:/sbin/sh" > /etc/passwd;
 
 # set timezone
 TZ=UTC
@@ -57,14 +28,7 @@ export TZ
 chown 0:0 /data/crontab/cron-scripts/*;
 chmod 777 /data/crontab/cron-scripts/*;
 
-# use /system/etc/cron.d/crontabs/ call the crontab file "root" for JB ROMS
-# use /var/spool/cron/crontabs/ call the crontab file "root" for ICS ROMS
+# use /var/spool/cron/crontabs/ call the crontab file "root"
 if [ -e /system/xbin/busybox ] || [ -e /system/bin/busybox ]; then
-	if [ "$JB_SAMMY" -eq "1" ]; then
 		nohup /system/xbin/busybox crond -c /var/spool/cron/crontabs/
-	elif [ "$JELLY" -eq "1" ]; then
-		nohup /system/xbin/busybox crond -c /system/etc/cron.d/crontabs/
-	else
-		nohup /system/xbin/busybox crond -c /var/spool/cron/crontabs/
-	fi;
 fi;
