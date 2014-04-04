c07797d139ae3e389917498a0cd771292cb78a21
dorimanx <yuri@bynet.co.il>
2014-04-04 23:46:37 +0300

    ONDEMAND: Boot tuning.

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -97,13 +97,12 @@ ONDEMAND_TUNING()
 {
 	echo "10" > /cpugov/ondemand/down_differential;
 	echo "10" > /cpugov/ondemand/down_differential_multi_core;
-	echo "1" > /cpugov/ondemand/enable_turbo_mode;
-	echo "70" > /cpugov/ondemand/micro_freq_up_threshold;
-	echo "1" > /cpugov/ondemand/sampling_down_factor;
+	echo "80" > /cpugov/ondemand/micro_freq_up_threshold;
+	echo "5" > /cpugov/ondemand/sampling_down_factor;
 	echo "60000" > /cpugov/ondemand/sampling_rate;
-	echo "60" > /cpugov/ondemand/up_threshold;
-	echo "60" > /cpugov/ondemand/up_threshold_any_cpu_load;
-	echo "60" > /cpugov/ondemand/up_threshold_multi_core;
+	echo "70" > /cpugov/ondemand/up_threshold;
+	echo "70" > /cpugov/ondemand/up_threshold_any_cpu_load;
+	echo "70" > /cpugov/ondemand/up_threshold_multi_core;
 }
 
 # oom and mem perm fix
