eed75ce8996a270fd52cf4b3569fa44e20f7b608
dorimanx <yuri@bynet.co.il>
2014-04-21 18:44:13 +0300

    Updated Busybox with KK support and removed install to SBIN.
    Rom now have SElinux and has to use it's own stuff for NET.
    
    So i have only critical needed applets in sbin for my scripts.

--- sbin/ext/busybox.sh
+++ sbin/ext/busybox.sh
@@ -6,8 +6,7 @@
 # installe latest busybox to ROM
 cp /sbin/busybox /system/xbin/;
 /system/xbin/busybox --install -s /system/xbin/
-/system/xbin/busybox --install -s /sbin/
-chmod 755 /system/xbin/busybox;
+chmod 06755 /system/xbin/busybox;
 
 /sbin/busybox sh /sbin/ext/post-init.sh;
 
