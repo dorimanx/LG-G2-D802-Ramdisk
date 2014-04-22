36307a72d1d4751acee423817faa68a4b55e0bdf
dorimanx <yuri@bynet.co.il>
2014-04-22 17:47:22 +0300

    Force perm fix on SU and daemonsu after busybox install. in xbin.

--- sbin/ext/busybox.sh
+++ sbin/ext/busybox.sh
@@ -1,12 +1,30 @@
-#!/sbin/sh
+#!/sbin/busybox sh
 
-/sbin/busybox mount -o remount,rw /system;
-/sbin/busybox mount -o remount,rw /;
+BB=/sbin/busybox
+
+$BB mount -o remount,rw /system;
+$BB mount -o remount,rw /;
 
 # installe latest busybox to ROM
-cp /sbin/busybox /system/xbin/;
+$BB cp /sbin/busybox /system/xbin/;
+if [ -e /system/xbin/su ]; then
+	$BB mv /system/xbin/su /system/
+fi;
+if [ -e /system/xbin/daemonsu ]; then
+	$BB cp /system/xbin/daemonsu /system/;
+fi;
+
 /system/xbin/busybox --install -s /system/xbin/
 chmod 06755 /system/xbin/busybox;
 
-/sbin/busybox sh /sbin/ext/post-init.sh;
+if [ -e /system/su ]; then
+	$BB mv /system/su /system/xbin/;
+	chmod 06755 /system/xbin/su;
+fi;
+if [ -e /system/daemonsu ]; then
+	$BB mv /system/daemonsu /system/xbin/daemonsu;
+	chmod 06755 /system/xbin/daemonsu;
+fi;
+
+$BB sh /sbin/ext/post-init.sh;
 
