db0acb5d125a907e0ba29406a3a31d20431816d6
Dorimanx <yuri@bynet.co.il>
2014-10-07 04:44:42 +0300

    Fixes for DEV and STweaks + Synapse.

--- ROOT-RAMDISK/sbin/ext/post-init.sh
+++ ROOT-RAMDISK/sbin/ext/post-init.sh
@@ -69,6 +69,7 @@ if [ ! -e /cpufreq ]; then
 	$BB ln -s /sys/kernel/alucard_hotplug/ /hotplugs/alucard;
 	$BB ln -s /sys/kernel/intelli_plug/ /hotplugs/intelli;
 	$BB ln -s /sys/module/msm_hotplug/ /hotplugs/msm_hotplug;
+	$BB ln -s /sys/devices/system/cpu/cpufreq/all_cpus/ /all_cpus;
 fi;
 
 # cleaning
