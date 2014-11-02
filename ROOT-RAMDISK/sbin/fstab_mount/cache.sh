a5de68a5ddeafdbe87bd66689707df8f6f2419a5
Dorimanx <yuri@bynet.co.il>
2014-11-02 10:46:49 +0200

    F2FS: cache not support labeling
    <7>SELinux: initialized (dev mmcblk0p31, type f2fs), not configured for labeling

--- ROOT-RAMDISK/sbin/fstab_mount/cache.sh
+++ ROOT-RAMDISK/sbin/fstab_mount/cache.sh
@@ -5,7 +5,7 @@ BB=/sbin/busybox
 CACHE=$($BB blkid /dev/block/platform/msm_sdcc.1/by-name/cache | $BB grep "f2fs" | $BB wc -l)
 
 if [ "${CACHE}" -eq "1" ]; then
-	$BB mount -t f2fs /dev/block/platform/msm_sdcc.1/by-name/cache /cache -o seclabel,nosuid,nodev;
+	$BB mount -t f2fs /dev/block/platform/msm_sdcc.1/by-name/cache /cache -o nosuid,nodev;
 else
 	$BB mount -t ext4 /dev/block/platform/msm_sdcc.1/by-name/cache /cache -o seclabel,nosuid,nodev,noauto_da_alloc,errors=continue;
 fi;
