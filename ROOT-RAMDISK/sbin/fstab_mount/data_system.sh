6cee0cd0da8a408fe54b932f0548f9f5bb579e47
Dorimanx <yuri@bynet.co.il>
2015-03-29 10:11:35 +0300

    Now that sepolicy support F2FS. we can mount using FSTAB file.
    Thanks to @blastagator for testing that!
    
    http://forum.xda-developers.com/showpost.php?p=59751515&postcount=13514

--- ROOT-RAMDISK/sbin/fstab_mount/data_system.sh
+++ ROOT-RAMDISK/sbin/fstab_mount/data_system.sh
@@ -9,21 +9,19 @@ SYSTEM_TYPE=0;
 DATA_TYPE=0;
 
 if [ "$SYSTEM" -eq "1" ]; then
-	$BB mount -t f2fs -o seclabel /dev/block/platform/msm_sdcc.1/by-name/system /system;
 	SYSTEM_TYPE=1;
 fi;
 if [ "$DATA" -eq "1" ]; then
-	$BB mount -t f2fs -o seclabel,nosuid,nodev /dev/block/platform/msm_sdcc.1/by-name/userdata /data;
 	DATA_TYPE=1;
 fi;
 
 $BB mount -o remount,rw /;
 
 if [ "$SYSTEM_TYPE" -eq "1" ] && [ "$DATA_TYPE" -eq "1" ]; then
-	$BB cp -p /sbin/fstab_mount/fstab_data_system.g2 /fstab.g2;
+	$BB cp -p /sbin/fstab_mount/fstab_f2fs_system_data.g2 /fstab.g2;
 elif [ "$SYSTEM_TYPE" -eq "1" ] && [ "$DATA_TYPE" -eq "0" ]; then
-	$BB cp -p /sbin/fstab_mount/fstab_system.g2 /fstab.g2;
+	$BB cp -p /sbin/fstab_mount/fstab_f2fs_system.g2 /fstab.g2;
 elif [ "$SYSTEM_TYPE" -eq "0" ] && [ "$DATA_TYPE" -eq "1" ]; then
-	$BB cp -p /sbin/fstab_mount/fstab_data.g2 /fstab.g2;
+	$BB cp -p /sbin/fstab_mount/fstab_f2fs_data.g2 /fstab.g2;
 fi;
 
