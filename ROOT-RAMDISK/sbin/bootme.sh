acd37c16348e1ca566b47e9474e7fec4df49b493
Dorimanx <yuri@bynet.co.il>
2015-04-04 00:59:00 +0300

    BOOTME SCRIPT: add turn off screen for kernel FS sync before boot.
    bootme.sh is used by me to restart to recovery super quick. :)
    
    then i do install of new build for testing.

--- ROOT-RAMDISK/sbin/bootme.sh
+++ ROOT-RAMDISK/sbin/bootme.sh
@@ -1,10 +1,11 @@
 #!/sbin/busybox sh
 
+input keyevent 26
+sync
+sync
 stop
-sync
-sync
 /sbin/busybox mount -o remount,ro /system;
 echo "rebooting to recovery now"
-sleep 2;
+sleep 3;
 reboot recovery
 
